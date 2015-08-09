2afe01e455e5ebcf379ee47e8725dbd8fa560bd9
Raghav Sai Cheedalla <raghavsaicheedalla@Raghavs-MacBook-Pro.local>
2015-08-08 19:26:38 -0700

    Initial Commit

---
+++ ChatApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatApplication
+//
+//  Created by Raghav Sai Cheedalla on 8/8/15.
+//  Copyright (c) 2015 Raghav Sai Cheedalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
